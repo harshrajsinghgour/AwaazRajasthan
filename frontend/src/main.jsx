30c4a615bd37f682f6e461be77c0d747c771748f
Harshraj singh gour <harshrajsinghgour1@gmail.com>
2026-09-24 09:43:01 +0530

    chore: trigger final production release

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-/* Awaaz Rajasthan production release trigger */
+/* Awaaz Rajasthan final production release trigger */
 import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./AppProduction";
